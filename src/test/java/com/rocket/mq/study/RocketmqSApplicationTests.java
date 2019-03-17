709851e01fb63e178dcbcee681003b04459150af
zhanglailiang <zhanglailiang@lvmama.com>
2019-03-17 15:11:10 +0800

    add

---
+++ src/test/java/com/rocket/mq/study/RocketmqSApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rocket.mq.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RocketmqSApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
